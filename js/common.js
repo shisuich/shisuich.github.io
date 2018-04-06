e18784df9a9e9d61a8a51919ab1f018c6560e396
goha <gohavorsin@gmail.com>
2018-04-07 03:03:38 +1000

    Change

--- js/common.js
+++ js/common.js
@@ -37,4 +37,17 @@ $(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
+	$(function() {
+    $('.menu__icon').on('click', function() {
+      $('.menu__icon').toggleClass('menu_state_open');
+      $('.menu_mobile').toggleClass('menu_open');
+    });
+  });
+  $(function() {
+    $('.link_mobile').on('click', function() {
+    	$('.menu__icon').removeClass('menu_state_open');
+      $('.menu_mobile').removeClass('menu_open');
+    });
+  });
+
 });
